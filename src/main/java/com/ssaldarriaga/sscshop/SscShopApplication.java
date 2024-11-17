032beced6c5bea213b1d10e065cecfcfc4ee85af
Sebastian Saldarriaga <ssaldarriaga.cardona@udea.edu.co>
2024-11-17 15:21:04 -0500

    first commit

---
+++ src/main/java/com/ssaldarriaga/sscshop/SscShopApplication.java
@@ -0,0 +1,13 @@
+package com.ssaldarriaga.sscshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SscShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SscShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ssaldarriaga/sscshop/SscShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssaldarriaga.sscshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SscShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
